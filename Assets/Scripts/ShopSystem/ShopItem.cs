2121820c8c2eea0e8e9fe05af475a6fe1c89579b
Nestor Santiago Plata Gomez <nestorsantiago.platagomez@gmail.com>
2024-11-27 15:09:12 -0500

    Implemented the father death sprite

--- Assets/Scripts/ShopSystem/ShopItem.cs
+++ Assets/Scripts/ShopSystem/ShopItem.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/ShopItem", order = 1)]
 public class ShopItem : ScriptableObject
 {
-    public string itemName;
-    public string itemDescription;
+    public string itemName = "Affects the price of food";
+    public string itemDescription = "Cheapens food price";
     public int cost;
     public bool available;
     public Sprite itemIcon;
@@ -16,35 +17,7 @@ public class ShopItem : ScriptableObject
     [Range(1,2)]public int ClothingLevel;
     public FamilyRole Owner;
 
-    void Start()
 
-    {
-        //// Accessing properties of a shop item
-        //ShopItem firstItem = shopItems[0];
-        //firstItem.itemName.Visibility = Visibility.Visible;
-        //firstItem.itemName.Text = $"Item Name: {firstItem.itemName}";
-        //firstItem.itemDescription.Visibility = Visibility.Visible;
-        //firstItem.itemDescription.Text = $"Item Description: {firstItem.itemDescription}";
-        //firstItem.cost.Visibility = Visibility.Visible;
-        //firstItem.itemDescription.Text = $"Item Cost: {firstItem.cost} ";
-        //// ... and so on
-
-        //firstItem.nextUpgrade.Visibility = Visibility.Hidden;
-        //// Checking for upgrades
-        //if (firstItem.nextUpgrade != null)
-        //{
-        //    firstItem.nextUpgrade.Visibility = Visibility.Visible;
-        //    firstItem.nextUpgrade.itemName.Text = $"Next Upgrade: {firstItem.nextUpgrade.itemName}";
-        //}
-
-        //firstItem.isFinalUpgrade.Visibility = Visibility.Hidden;
-        //// Checking if it's a final upgrade
-        //if (firstItem.isFinalUpgrade)
-        //{
-        //    firstItem.isFinalUpgrade.Visibility = Visibility.Visible;
-        //    firstItem.isFinalUpgrade.Text = "This is the final upgrade.";
-        //}
-    }
     public int GetFinalClothingUpgradeLevel()
     {
         if(nextUpgrade)
