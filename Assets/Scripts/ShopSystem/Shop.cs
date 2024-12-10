2101a2e12df7b13f36fc6bbebddefc19fd06fc15
Nestor Santiago Plata Gomez <62219726+nestorplata@users.noreply.github.com>
2024-12-09 21:22:40 -0500

    Implementing animations

--- Assets/Scripts/ShopSystem/Shop.cs
+++ Assets/Scripts/ShopSystem/Shop.cs
@@ -2,23 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Windows;
 //using static UnityEditor.Progress;
 
 public class Shop : MonoBehaviour
 {
-    [SerializeField]
-    private ClothingItem[] shopItems;
 
+
+    
     [SerializeField]
     private GameObject shopIcon;
 
-    [SerializeField]
-    private GameObject vertContainer;
+    private List<PanelItems> ShopPanels;
 
 
     private ShopUI shop;
 
-    public List<Transform> Shelves;
 
     ////TESTING VARS
     //private int aliveFamily = 0;
@@ -27,24 +26,37 @@ public class Shop : MonoBehaviour
     // Start is called before the first frame update
     public void Start()
     {
-        int currentDay = familyScript.Instance.getDay();
-        foreach(Transform Childs in vertContainer.transform)
+
+        ShopPanels.Add(transform.GetComponentInChildren<PanelItems>());
+
+        foreach (var panel in ShopPanels)
+        {
+            InstantiateShopPanel(panel);
+        }
+    }
+
+    public void InstantiateShopPanel(PanelItems ShopPanel)
+    {
+        List<Transform> Shelves = new List<Transform>();
+
+
+        foreach (Transform Childs in ShopPanel.transform)
         {
             Shelves.Add(Childs);
         }
         int x = 0;
         int y = 0;
-        foreach (ClothingItem ShopItem in shopItems)
+        foreach (ClothingItem ShopItem in ShopPanel.shopItems)
         {
             //Setup
             familyMember member = familyScript.Instance.GetFamilyMember(ShopItem.Owner);
-            if(!member.ShopItem)
+            if (!member.ShopItem)
             {
                 member.ShopItem = ShopItem;
             }
             if (!member.IsDead())
             {
-                if(x==2)
+                if (x == 2)
                 {
                     x = 0;
                     y++;
@@ -54,7 +66,7 @@ public class Shop : MonoBehaviour
                 //newItem.transform.localPosition = new Vector3(500, 100 * i, 0);
                 //    i++;
                 ShopUI icon = newItem.GetComponent<ShopUI>();
-                
+
                 if (member.ClothingLevel >= member.ShopItem.GetFinalUpgradeLevel())
                 {
                     icon.SetupSoldOut(member.ShopItem);
@@ -64,64 +76,7 @@ public class Shop : MonoBehaviour
                     icon.Setup(member.ShopItem);
                 }
 
-                //if (currentDay == 1)
-                //{
-                //    //Reset everything
-                //    icon.Setup(ShopItem);
-                //    icon.SetAvailability(true);
-                //    //PlayerPrefs.DeleteAll();
-                //    //PlayerPrefs.Save();
-                //}
-                //else
-                //{
-
-
-                //    //switch()
-                //    // icon.SetAvailability(savedAvailability);
-
-                //}
             }
-
-
         }
-
-        /* var i = 0; //TEMP
-         aliveFamily = Random.Range(1, 5);
-         sickFamily = Random.Range(0, aliveFamily);
-
-         foreach (ShopItem shop in shopItems)
-         {
-             //Setup
-             GameObject newItem = Instantiate(shopIcon);
-             ShopUI icon = newItem.GetComponent<ShopUI>();
-
-             //Create new slot
-             icon.itemName.text = shop.itemName;
-
-
-             //LOGIC PER ITEM (TEMP)
-             if (i == 0)//Food
-             {
-                 icon.itemCost.text = (shop.cost* aliveFamily).ToString();
-                 newItem.transform.parent = vertContainer.transform;
-             }
-             else //Med
-             {
-                 icon.itemCost.text = (shop.cost * sickFamily).ToString();
-                 if (sickFamily > 0)
-                 {
-                     newItem.transform.parent = vertContainer.transform;
-                 }
-                 else
-                 {
-                     Destroy(newItem);
-                 }
-             }
-             icon.itemIcon.sprite = shop.itemIcon;
-             i++;
-         }
-
-         Debug.Log("alive "+aliveFamily);
-         Debug.Log("sick "+sickFamily);*/
     }
 }
